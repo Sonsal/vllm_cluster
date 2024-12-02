7661033af100d2487ff0d8b35ee004eb0dc1c4a5
Sonsal <fax.dig@yandex.ru>
2024-12-02 03:19:00 +0300

    dev: added sglang support

--- sglang/server.sh
+++ sglang/server.sh
@@ -1,1 +1,1 @@
-python3 -m sglang.launch_server --model-path Qwen/Qwen2.5-3B-Instruct --port 8000 --host 0.0.0.0 --served-model-name SGLANG/Assistant-model
+python3 -m sglang.launch_server --model-path Qwen/Qwen2.5-3B-Instruct --port 8000 --host 0.0.0.0 --served-model-name SGLANG/Assistant-model --enable-metrics --chunked-prefill-size 4096 
